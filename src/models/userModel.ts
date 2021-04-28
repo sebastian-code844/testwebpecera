6c4386b0060fc2e260fba7cc597525bcc23a69c9
Ezequiel Esquivel <ezequielesquivel@gmail.com>
2021-04-28 18:18:40 -0300

    coneccion bd

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -8,9 +8,9 @@ class UserModel {
 
 	async config() {//Parametro de conexion con la BD.
 		this.db = await createPool({
-			host: 'localhost',
-			user: 'root',
-			password:'',
+			host: 'us-cdbr-east-03.cleardb.com',
+			user: 'b0e0fd43ed8818',
+			password:'2b1f9d39',
 			database: 'heroku_4505cc56058eb11',
 			connectionLimit: 10
 		});
